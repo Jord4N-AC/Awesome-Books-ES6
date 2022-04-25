e69808d79358d0d689f55934499c5039a2671239
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-25 16:51:14 -0500

    Fix linter errors

--- modules/alert_message.js
+++ modules/alert_message.js
@@ -2,8 +2,8 @@ import { alertMessage } from './varibles.js';
 
 export const highLightMessage = () => {
   alertMessage.style.transform = 'scale(1.15)';
-}
+};
 
 export const noHighlightMessage = () => {
   alertMessage.style.transform = 'scale(1)';
-}
+};
