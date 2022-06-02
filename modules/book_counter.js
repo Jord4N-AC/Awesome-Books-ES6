b8eb9575debc50dc582b919b5d32ed4c2c51f189
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-06-02 10:59:49 -0500

    Refactor to use arrow functions on modules/book_counter.js

--- modules/book_counter.js
+++ modules/book_counter.js
@@ -1,8 +1,8 @@
 // Show a counter next to link1 after adding a book
-export function showBookCounter(
+export const showBookCounter = (
   oldLength, newLegth, bookCounter,
   allRecent = document.querySelectorAll('.recent-added'),
-) {
+) => {
   bookCounter.classList.remove('highlight-counter');
   if (newLegth > oldLength) {
     bookCounter.innerHTML = allRecent.length;
@@ -12,7 +12,7 @@ export function showBookCounter(
 }
 
 // Remove counter when clicking link1
-export function hideBookCounter(navLinks, bookCounter, hash = window.location.hash) {
+export const hideBookCounter = (navLinks, bookCounter, hash = window.location.hash) => {
   if (hash === navLinks[0].getAttribute('href')) {
     bookCounter.style.display = 'none';
   }
