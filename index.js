ec8ab7402e538d5224c9d0a62f58bf21f3150c5b
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-02-20 20:44:51 -0500

    Add luxon.min.js and import it to use

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { DateTime } from './node_modules/luxon/src/luxon.js';
+import { DateTime } from './modules/luxon.min.js';
 import {
   bookList, inputTitle, inputAuthor, addButton, navLinks, sections,
   alertMessage, successMessage, bookCounter, date,
